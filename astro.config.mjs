5136f1017f7cb3f470079a103b5a385e578d7351
Simen A. W. Olsen <so@bjerk.io>
2024-11-25 13:37:04 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  redirects: {
+    "/bestill/prosjektrapporten": "https://tally.so/r/3y0GDd",
+  },
+});
